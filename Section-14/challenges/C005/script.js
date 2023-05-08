bad36ea58d0da296ecdbe9c7bd401382d94e0907
unknown <studyadailo@gmail.com>
2023-05-08 10:40:05 -0300

    chall005

---
+++ Section-14/challenges/C005/script.js
@@ -0,0 +1,22 @@
+function getMilk(cash){
+    var bottles = Math.floor(cash / 1.5); //No float
+    alert("Leave house");
+    alert("move Right");
+    alert("move Right");
+    alert("moveUp");
+    alert("moveUp");
+    alert("moveUp");
+    alert("moveUp");
+    alert("move Right");
+    alert("move Right");
+    alert("Buy " + bottles + " bottles of milk");
+    alert("move Left");
+    alert("move Right");
+    alert("move Down");
+    alert("move Down");
+    alert("move Down");
+    alert("move Down");
+    alert("move Left");
+    alert("move Left");
+    alert("Enter house");
+}
