bfd234276ff18265a5424870e20975e29fb2d679
unknown <studyadailo@gmail.com>
2023-07-12 13:43:01 -0300

    BMI Calculator

--- section-24/calculator/calculator.js
+++ section-24/calculator/calculator.js
@@ -16,6 +16,19 @@ app.post("/", function(req, res){
     res.send("Thaks for send it, the result is: " + result);
 })
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    var weight = parseFloat(req.body(weight));
+    var height = parseFloat(req.body(height));
+    var bmi = weight / (height * height);
+
+    res.send("Bmi: " + bmi);
+});
+
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
